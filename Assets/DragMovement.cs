62d84f299b45b5cfc037671040e470a4c32060aa
Alex Lopez <aloro2392@gmail.com>
2023-01-24 02:13:48 +0100

    Adding water

--- Assets/DragMovement.cs
+++ Assets/DragMovement.cs
@@ -7,6 +7,7 @@ public class DragMovement : MonoBehaviour
     public float forceMultiplier = 10f;
     private Rigidbody2D rb;
     private LineRenderer lineRenderer;
+    private Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,12 +35,27 @@ public class DragMovement : MonoBehaviour
         {
             float distance = Vector3.Distance(worldPos, transform.position);
             Vector3 force = (worldPos - transform.position).normalized * distance * forceMultiplier;
+            lastPosition = transform.position;
             rb.AddForce(force);
         } else if (Input.GetMouseButtonUp(1))
         {
             float distance = Vector3.Distance(worldPos, transform.position);
             Vector3 force = (worldPos - transform.position).normalized * distance * forceMultiplier;
+            lastPosition = transform.position;
             rb.AddForce(-force);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Comprueba si el objeto con el que colisiona la pelota tiene el material WaterMaterial
+        if (collider.gameObject.GetComponent<Renderer>().sharedMaterial.name == "WaterMaterial")
+        {
+            // Aquí puedes escribir el código que desees ejecutar cuando la pelota choca con el cubo con el material WaterMaterial.
+            // Por ejemplo, volver a la posición desde donde se golpeó
+            Vector3 startingPosition = new Vector3(0, 0, 0);
+            transform.position = lastPosition;
+            rb.velocity = new Vector2(0, 0);
         }
     }
 }
