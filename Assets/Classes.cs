c102ed42999417273625ba5be0077ebd6fc88641
Alex Lopez <aloro2392@gmail.com>
2023-01-27 16:21:08 +0100

    esto ya no se va a usar

--- Assets/Classes.cs
+++
@@ -1,6 +0,0 @@
-public class Classes
-{
-    // definir aqui la estructura de la clase, q será abstracta
-    // la idea es que hayan diferentes clases como en un rpg
-    // warrior, archer, etc. cada una con sus ataques, fortalezas y debilidades
-}
